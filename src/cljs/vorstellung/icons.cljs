5f089ab7d39d81fa48538b782707bba85b779173
kimim <kimi.im@outlook.com>
2020-11-25 15:25:02 +0800

    display 90 icons out of 5545 total icons

--- src/cljs/vorstellung/icons.cljs
+++ src/cljs/vorstellung/icons.cljs
@@ -1,10 +1,17 @@
 (ns vorstellung.icons
-  (:require ["@material-ui/icons" :as icons  :refer [Apps Mail Cake BugReport]]))
+  (:require ["@material-ui/core" :as m]
+            ["@material-ui/icons" :as icons]))
 
 (defn page []
-  [:main {:style {:flexGrow 1 :padding "88px 24px 24px 24px"}}
-   [:div
-    [:> Apps]
-    [:> Mail]
-    [:> Cake]
-    [:> BugReport]]])
+  (let [icons (goog.object/getValues icons)
+        count (goog.object/getCount icons)]
+    [:main {:style {:flexGrow 1 :padding "88px 24px 24px 24px"}}
+     [:div
+      [:> m/Grid {:container true :spacing 3}
+       (for [i (range 90)]
+         (let [icon (aget icons (rand-int count))]
+           [:> m/Grid {:item true :xs 2}
+            [:> m/Paper
+             [:div
+              [:> icon]
+              [:p (aget icon "displayName")]]]]))]]]))
