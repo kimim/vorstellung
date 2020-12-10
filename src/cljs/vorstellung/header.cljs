ec096cfede746d07fad75d376a1a86131a9f5424
kimim <kimi.im@outlook.com>
2020-12-10 13:46:11 +0800

    add a button to hide menu and navbar

--- src/cljs/vorstellung/header.cljs
+++ src/cljs/vorstellung/header.cljs
@@ -16,6 +16,8 @@
    ["@material-ui/icons/GridOn" :default GridOn]
    ["@material-ui/icons/BackupOutlined" :default BackupOutlined]
    ["@material-ui/icons/Today" :default Today]
+   ["@material-ui/icons/Fullscreen" :default Fullscreen]
+   ["@material-ui/icons/FullscreenExit" :default FullscreenExit]
    #_["@material-ui/icons"
     :refer [Menu ChevronRight ChevronLeft Toys ViewModule Info]]))
 
@@ -25,38 +27,53 @@
    [:> m/Typography {:variant "inherit" :noWrap true} text]])
 
 (defn navbar []
-  (r/with-let [open (r/atom false)]
-    [:div
-     [:> m/AppBar {:position "fixed"
-                   :style {:width (if @open "calc(100% - 240px)" "100%")
-                           :z-index 2000}}
-      [:> m/Toolbar {:classes {:root "appBar"}}
-       [:> m/IconButton {:color "inherit"
-                         :style {:margin-left "-20px"
-                                 :margin-right "2px" :display (if @open "none" "inherit")}
-                         :on-click #(swap! open not)}
-        [:> Menu]]
-       [:> m/Typography {:variant "h6" :style {:flexGrow 1}}
-        "Die Welt ist Meine Vorstellung"]
-       [:> m/Link  {:href "/logout"}
-        [:> m/Avatar {:style {:backgroundColor "orange"}}
-         [:> ExitToApp]]]]]
-     [:> m/Drawer {:variant "permanent"
-                   :classes {:paper (if @open "drawer-open" "drawer-close")}
-                   :style {:width (if @open "240px" "55px")
-                           :overflow-x "hidden"}}
-      [:div {:style {:align-items "center" :min-height 64
-                     :display "flex" :justifyContent "flex-end"}}
-       [:> m/IconButton {:style {:height 48 :width 48}
-                         :on-click #(swap! open not)}
-        (if @open [:> ChevronLeft]
-            [:> ChevronRight])]]
-      [:> m/Divider]
-      [:> m/List {:style {:width (if @open "240px" "55px")}}
-       [menu-item "/icons" ToysOutlined "Icons"]
-       [menu-item "/material/#/grid" ViewComfy "Grid Layout"]
-       [menu-item "/material/#/data-grid" GridOn "Data Grid"]
-       [menu-item "/material/#/upload" BackupOutlined "File Uploader"]
-       [menu-item "/material/#/picker" Today "Data Picker"]
-       [menu-item "/charts" InsertChartOutlined "BizCharts"]
-       [menu-item "/#/about" ContactSupportOutlined "About"]]]]))
+  (r/with-let [open (r/atom false)
+               hide (r/atom false)]
+    (condp
+      = @hide
+      false
+      [:div
+       [:> m/AppBar {:position "fixed"
+                     :style {:width (if @open "calc(100% - 240px)" "100%")
+                             :z-index 2000}}
+        [:> m/Toolbar {:classes {:root "appBar"}}
+         [:> m/IconButton {:color "inherit"
+                           :style {:margin-left "-20px"
+                                   :margin-right "2px" :display (if @open "none" "inherit")}
+                           :on-click #(swap! open not)}
+          [:> Menu]]
+         [:> m/Typography {:variant "h6" :style {:flexGrow 1}}
+          "Die Welt ist Meine Vorstellung"]
+         [:> m/IconButton {:style {:color "white"}
+                           :on-click #(swap! hide not)}
+          [:> Fullscreen]]
+         [:> m/Link  {:href "/logout"}
+          [:> m/IconButton {:style {:color "white"}}
+           [:> ExitToApp]]]]]
+       [:> m/Drawer {:variant "permanent"
+                     :classes {:paper (if @open "drawer-open" "drawer-close")}
+                     :style {:width (if @open "240px" "55px")
+                             :overflow-x "hidden"}}
+        [:div {:style {:align-items "center" :min-height 64
+                       :display "flex" :justifyContent "flex-end"}}
+         [:> m/IconButton {:style {:height 48 :width 48}
+                           :on-click #(swap! open not)}
+          (if @open [:> ChevronLeft]
+              [:> ChevronRight])]]
+        [:> m/Divider]
+        [:> m/List {:style {:width (if @open "240px" "55px")}}
+         [menu-item "/icons" ToysOutlined "Icons"]
+         [menu-item "/material/#/grid" ViewComfy "Grid Layout"]
+         [menu-item "/material/#/data-grid" GridOn "Data Grid"]
+         [menu-item "/material/#/upload" BackupOutlined "File Uploader"]
+         [menu-item "/material/#/picker" Today "Data Picker"]
+         [menu-item "/charts" InsertChartOutlined "BizCharts"]
+         [menu-item "/#/about" ContactSupportOutlined "About"]]]]
+
+      true
+      [:> m/IconButton {:style {:color "blue"
+                                :position "absolute"
+                                :right "72px"
+                                :top "9px"}
+                           :on-click #(swap! hide not)}
+       [:> FullscreenExit]])))
