e5b92b089d23818ec0eb36b9bd3b75435be56694
kimim <kimi.im@outlook.com>
2020-12-23 21:55:42 +0800

    try to lein uberjar

--- project.clj
+++ project.clj
@@ -5,4 +5,14 @@
             :url "https://opensource.org/licenses/MIT"}
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]})
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :main ^:skip-aot vorstellung.core
+  :profiles
+  {:uberjar {:omit-source true
+             :prep-tasks ["compile" #_["shadow" "release" "app"]]
+             :aot :all
+             :uberjar-name "vorstellung.jar"
+             :source-paths ["src/clj" "src/cljs" "src/cljc"
+                            "env/prod/clj" "env/prod/cljs" ]
+             :resource-paths ["resources" "target/cljsbuild"
+                              "env/prod/resources"]}})
