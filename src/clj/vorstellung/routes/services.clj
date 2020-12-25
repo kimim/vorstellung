f6c3bbce47cad857b0508f48f17cd2b5d413e099
kimim <kimi.im@outlook.com>
2020-12-25 08:51:40 +0800

    add dogs API

--- src/clj/vorstellung/routes/services.clj
+++ src/clj/vorstellung/routes/services.clj
@@ -44,16 +44,63 @@
 
     ["/api-docs/*"
      {:get (swagger-ui/create-swagger-ui-handler
-             {:url "/api/swagger.json"
-              :config {:validator-url nil}})}]]
+            {:url "/api/swagger.json"
+             :config {:validator-url nil}})}]]
 
    ["/ping"
     {:get (constantly (ok {:message "pong"}))}]
 
-   ["/edn"
-    {:get {:handler
-           {:status 200
-            :body {:A 123}}}}]
+   ["/dogs"
+    {:swagger {:tags ["dogs"]}}
+    [""
+     {:get {:summary "fetch all dogs info with params"
+            :parameters {}
+            :response {200 {:body {:dogs [{:dog {:id int? :name string? :color string?}}]}}}
+            :handler (fn [{{{:keys [id]} :path} :parameters}]
+                       {:status 200
+                        :body {:dogs
+                               [{:dog
+                                 {:id 1234
+                                  :name "AI"
+                                  :color "brwon"}}
+                                {:dog
+                                 {:id 1234
+                                  :name "AI"
+                                  :color "brwon"}}]}})}
+      :post {:summary "add a new dog"
+             :parameters {:query {:name string? :color string?}}
+             :response {200 {:body {:dogs [{:dog {:id int? :name string? :color string?}}]}}}
+             :handler (fn [{{{:keys [name color]} :query} :parameters}]
+                        {:status 200
+                         :body {:dog
+                                {:id 1234
+                                 :name name
+                                 :color color}}})}}]
+    ["/:id"
+     {:get {:summary "fetch dog info with id"
+            :parameters {:path {:id int?}}
+            :response {200 {:body {:dog {:id int? :name string? :color string?}}}}
+            :handler (fn [{{{:keys [id]} :path} :parameters}]
+                       {:status 200
+                        :body {:dog
+                               {:id 1234
+                                :name "AI"
+                                :color "brwon"}}})}
+      :put {:summary "update a dog"
+            :parameters {:path {:id int?}
+                         :query {:name string? :color string?}}
+            :response {200 {:body {:dog {:id int? :name string? :color string?}}}}
+            :handler (fn [{{{:keys [id]} :path {:keys [name color]} :query} :parameters}]
+                       {:status 200
+                        :body {:dog
+                               {:id id
+                                :name name
+                                :color color}}})}
+      :delete {:summary "delete dog info with id"
+               :parameters {:path {:id int?}}
+               :response {200 {}}
+               :handler (fn [{{{:keys [id]} :path} :parameters}]
+                          {:status 200})}}]]
 
    ["/math"
     {:swagger {:tags ["math"]}}
@@ -66,9 +113,9 @@
                        {:status 200
                         :body {:total (+ x y)}})}
       :post {:summary "plus with spec body parameters"
-             :parameters {:body {:x int?, :y int?}}
+             :parameters {:query {:x int?, :y int?}}
              :responses {200 {:body {:total pos-int?}}}
-             :handler (fn [{{{:keys [x y]} :body} :parameters}]
+             :handler (fn [{{{:keys [x y]} :query} :parameters}]
                         {:status 200
                          :body {:total (+ x y)}})}}]]
 
